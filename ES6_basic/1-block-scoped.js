fc270650140e8df6e1cc783080c0e1b7e6f35974
kevxcyj <kevcyjunior@gmail.com>
2024-10-27 18:09:47 -0500

    Added number 1

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
+  const task = false;
+  const task2 = true;
 
 
 if (trueOrfalse) {
-    const task3 = true;
-    const task4 = false;
-    console.log('test output:', task3, task4);
+  const task3 = true;
+  const task4 = false;
+  console.log('test output:', task3, task4);
 }
 return [task, task2];
 }
