ced0bf2a729a82693079d4abfea74a099bd392f5
kevxcyj <kevcyjunior@gmail.com>
2024-10-30 09:26:11 -0500

    Added number 0

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-unused-vars
+export function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco'},
+    { id: 2, firstName: 'James', location: 'Columbia'},
+    { id: 5, firstName: 'Serena', location: 'San Francisco'}
+  ];
+}
