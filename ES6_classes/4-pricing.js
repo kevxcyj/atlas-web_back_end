e53a1231c95fa74810145fce9ce37280f236f364
kevxcyj <kevcyjunior@gmail.com>
2024-10-30 18:24:10 -0500

    Added number 4

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,44 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  // Amount setter
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amountvalue) {
+    if (typeof amountvalue !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = amountvalue;
+  }
+
+  // Currency setter
+  get currency() {
+    return this.currency;
+  }
+
+  set currency(currencyvalue) {
+    if (!(currencyvalue instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency class');
+    }
+    this._currency = currencyvalue;
+  }
+
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new TypeError('Amount and Conversion Rate must be numbers');
+    }
+    return conversionRate * amount;
+  }
+}
