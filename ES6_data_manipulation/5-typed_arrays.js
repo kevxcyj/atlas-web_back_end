8714eabdd0adf49a93d064ad6634ac32b68b8fee
kevxcyj <kevcyjunior@gmail.com>
2024-10-30 14:43:33 -0500

    Wrong file

--- ES6_data_manipulation/5-typed_arrays.js
+++
@@ -1,7 +0,0 @@
-export default function getStudentIdsSum(students) {
-    if (Object.getPrototypeOf(students) === Array.prototype) {
-        const ids = students.map((items) => items.id);
-        const reducer = (sum, currentValue) => currentValue;
-        return ids.reduce(reducer,  0);
-    }
-}
