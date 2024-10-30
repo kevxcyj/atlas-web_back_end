12403a38f81f67a1f5944b411cd14bbfdebc0fd5
kevxcyj <kevcyjunior@gmail.com>
2024-10-30 13:34:37 -0500

    Added number 2

---
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,7 @@
+import { getListStudents } from './0-get_list_students.js';
+
+export default function getStudentsByLocation(location) {
+  const students = getListStudents();
+
+  return students.filter(student => student.location === location);
+}
