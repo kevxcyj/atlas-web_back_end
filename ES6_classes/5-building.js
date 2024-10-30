650a21099a0f0853c6a19d3e08e0c324c28738c3
kevxcyj <kevcyjunior@gmail.com>
2024-10-30 18:36:01 -0500

    Added number 5

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,14 @@
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+
+
+    if (this.constructor !== Building && (this.evacuationWarningMessage === undefined)) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this.sqft;
+  }
+}
