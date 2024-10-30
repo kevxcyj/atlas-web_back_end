57ca922b2b69300bda973d3c7b3fd41d9b1ca5e0
kevxcyj <kevcyjunior@gmail.com>
2024-10-30 14:43:09 -0500

    Wrong file

---
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,7 @@
+export default function getStudentIdsSum(students) {
+  if (Object.getPrototypeOf(students) === Array.prototype) {
+      const ids = students.map((items) => items.id);
+      const reducer = (sum, currentValue) => currentValue;
+      return ids.reduce(reducer,  0);
+  }
+}
