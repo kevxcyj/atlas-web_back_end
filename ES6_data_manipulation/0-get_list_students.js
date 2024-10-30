ba5af1bbfaee81a45382f84d9d53b1f7a9faf254
kevxcyj <kevcyjunior@gmail.com>
2024-10-30 09:29:10 -0500

    Fixed spaces

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 export default function getListStudents() {
   return [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco'},
-    { id: 2, firstName: 'James', location: 'Columbia'},
-    { id: 5, firstName: 'Serena', location: 'San Francisco'}
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
   ];
 }
