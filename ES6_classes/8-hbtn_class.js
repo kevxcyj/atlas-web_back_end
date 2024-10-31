19014fbf3fd4b6a08edf60d155fd9b9755fb4375
kevxcyj <kevcyjunior@gmail.com>
2024-10-30 19:14:42 -0500

    Fixed lint

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -4,23 +4,25 @@ export default class HolbertonClass {
     this._location = location;
   }
 
-  get size() { 
-    return this._size; 
+  get size() {
+    return this._size;
   }
 
-  set size(newSize) { 
-    if (typeof newSize !== 'number' || newSize <= 0) 
-      throw new Error('Size must be a non-zero number'); this._size = newSize; 
+  set size(newSize) {
+    if (typeof newSize !== 'number' || newSize <= 0) {
+      throw new Error('Size must be a non-zero number'); this._size = newSize;
     }
+  }
   
-  get location() { 
-    return this._location; 
+  get location() {
+    return this._location;
   }
   
   set location(newLocation) {
-    if (typeof newLocation !== 'string' || newLocation.length <= 0) 
-      throw new Error('Location must be a non-empty string'); this._location = newLocation; 
+    if (typeof newLocation !== 'string' || newLocation.length <= 0) {
+      throw new Error('Location must be a non-empty string'); this._location = newLocation;
     }
+  }
 
   valueOf() {
     return this._size;
