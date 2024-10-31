25ea69b8fcfa923fc1f52c6a11cbf32b4f20066c
kevxcyj <kevcyjunior@gmail.com>
2024-10-30 19:58:17 -0500

    Fixed indention

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-    return promise.then(() => ({
-        body: 'success',
-        status: 200,
-    })).catch(() => Error()).finally(() => console.log('Got a response from the API'));
+  return promise.then(() => ({
+      body: 'success',
+      status: 200,
+  })).catch(() => Error()).finally(() => console.log('Got a response from the API'));
 }
