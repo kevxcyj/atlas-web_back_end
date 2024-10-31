2be76ee5afcad42808fba23c7a1244a1ab718914
kevxcyj <kevcyjunior@gmail.com>
2024-10-30 19:50:09 -0500

    Fixed bracktes

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -2,8 +2,8 @@ export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
       const value = { status: 200, body: 'Success' };
-      resolve(value); } 
-    else {
+      resolve(value); 
+    } else {
       reject(new TypeError('The fake API is not working currently'));
     }
   });
