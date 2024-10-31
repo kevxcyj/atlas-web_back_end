621c3fe65172dac0f8054f03a4c745719e50bfc1
kevxcyj <kevcyjunior@gmail.com>
2024-10-30 20:15:50 -0500

    added number 4

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,8 +1,8 @@
 export default function signupUser(firstName, lastName) {
-    const promise = new Promise((resolve) => {
-      resolve(
-        { firstName, lastName },
-      );
-    });
-    return promise;
-  }
+  const promise = new Promise((resolve) => {
+    resolve(
+      { firstName, lastName },
+    );
+  });
+  return promise;
+}
