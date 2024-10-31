b539dbcb2f8a26393d267d2c4a06f17e0ffa88ad
kevxcyj <kevcyjunior@gmail.com>
2024-10-30 19:47:36 -0500

    Added number one

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,11 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      const value = { status: 200, body: 'Success' };
+      resolve(value);
+    } 
+    else {
+      reject(new TypeError('The fake API is not working currently'));
+    }
+  });
+}
