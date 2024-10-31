7648f575819af496abd813bd86261c5011a596b2
kevxcyj <kevcyjunior@gmail.com>
2024-10-30 20:20:13 -0500

    Added number 6

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,19 @@
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPromise = uploadPhoto(fileName);
+
+  return Promise.all([signUpPromise, uploadPromise])
+    .then(([signupResult, uploadResult]) => {
+      return [
+        { status: signupResult.status, value: signupResult.value },
+        { status: uploadResult.status, value: uploadResult.value }
+      ];
+    })
+    .catch(error => {
+      console.error('Error during profile signup:', error);
+      throw error;
+    });
+}
