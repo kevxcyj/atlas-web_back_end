c08d1f251ce2d7838323bd668d744a997c3a01a0
kevxcyj <kevcyjunior@gmail.com>
2024-10-30 19:50:46 -0500

    whitespace

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -2,7 +2,7 @@ export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
       const value = { status: 200, body: 'Success' };
-      resolve(value); 
+      resolve(value);
     } else {
       reject(new TypeError('The fake API is not working currently'));
     }
