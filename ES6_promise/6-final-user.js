b10add5dbee1bea9b50f971848e41b0249b58006
kevxcyj <kevcyjunior@gmail.com>
2024-10-30 20:21:40 -0500

    Removed file path

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,5 +1,5 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export function handleProfileSignup(firstName, lastName, fileName) {
   const signUpPromise = signUpUser(firstName, lastName);
