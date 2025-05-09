e6a573fff38622e455e3936f4ecbf3b1ee9296fd
kevxcyj <kevcyjunior@gmail.com>
2025-05-09 12:43:36 -0500

    Added number 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+""" Concat function """
+
+def concat(str1: str, str2: str) -> str:
+    """ Concatenates two strings
+
+    Args:
+        str1: First string
+        str2: Second string
+
+    Returns:
+        The concatenated string
+    """
+    return str1 + str2
