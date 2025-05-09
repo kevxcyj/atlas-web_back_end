54695900a5347ef0cd11822786cff7fb72e0d7da
kevxcyj <kevcyjunior@gmail.com>
2025-05-09 12:32:48 -0500

    Added number 0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+""" Add function """
+
+def add(a: float, b: float) -> float:
+    """ Returns the sum of two floats
+    
+    Args:
+        a: First number
+        b: Second number
+
+    Returns:
+        The sum of 'a + b'
+    """
+
+    return a + b
