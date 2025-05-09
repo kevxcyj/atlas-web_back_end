deee628ac57fd04621c21c2ff939f927a733dcf3
kevxcyj <kevcyjunior@gmail.com>
2025-05-09 12:56:03 -0500

    Added number 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+""" Floor function """
+
+def floor(n:float) -> int:
+    """ Returns the floor of float
+
+    Args:
+        n: Float
+
+    Returns:
+        The floor of 'n'
+    """
+    return int(n)
