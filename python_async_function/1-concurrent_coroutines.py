1fc41c4deea74b1e41b844ea8e244e2b28a75690
kevxcyj <kevcyjunior@gmail.com>
2025-05-10 23:25:53 -0500

    pycode

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -9,7 +9,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawns wait_random n times with the specified max_delay 
+    """Spawns wait_random n times with the specified max_delay
     and returns the list of all the delays in ascending order.
 
     Args:
--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -9,7 +9,7 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawns wait_random n times with the specified max_delay 
+    """Spawns wait_random n times with the specified max_delay
     and returns the list of all the delays in ascending order.
 
     Args:
