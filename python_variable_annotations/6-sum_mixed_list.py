b8b1bdfdf7d72925ba85844c6952caff023d4cb3
kevxcyj <kevcyjunior@gmail.com>
2025-05-10 21:42:33 -0500

    Added number 6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+""" Returns sum of integers and floats """
+
+from typing import List, Union
+
+def sum_mixed_list(mxd_lst: List [Union[int, float]]) -> float:
+    """ Adds together values and returns sum 
+
+        Args:
+            mxd_list: Integers/floats
+
+        Returns:
+            Sum of Integers/floats
+    """
+
+    return sum(mxd_lst)
