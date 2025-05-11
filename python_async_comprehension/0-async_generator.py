e9538259b8b41c801ca25b52f731d6e12dbf416d
kevxcyj <kevcyjunior@gmail.com>
2025-05-10 23:31:37 -0500

    pycode

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,12 +8,13 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    """Loops 10 times, wait 1 second, then yield a random number between 0 and 10.
+    """Loops 10 times, wait 1 second,
+        then yield a random number between 0 and 10.
 
     Returns:
         Float between 1 and 0
     """
-    
+
     for i in range(10):
         await asyncio.sleep(1)
         yield random.random() * 10
--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -9,15 +9,17 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """ Executes async_comprehension four times in parallel using asyncio.gather.
-    Measures the total runtime and returns it.
+    """ Executes async_comprehension four times in parallel 
+        using asyncio.gather. Measures the total runtime 
+        and returns it.
 
     Returns:
        Runtime in seconds
     """
 
     start_time = time.time()
-    await asyncio.gather(async_comprehension(), async_comprehension(), async_comprehension(), async_comprehension())
+    await asyncio.gather(async_comprehension(), async_comprehension(), 
+                         async_comprehension(), async_comprehension())
     end_time = time.time()
 
     return end_time - start_time
