d5fb61bc6d7b4d6309c5d55c6675fb750148faf5
kevxcyj <kevcyjunior@gmail.com>
2025-05-10 22:11:43 -0500

    Added number 7

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+""" to_kv function """
+
+from typing import Tuple, Union
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """ Takes string k and/or float v and returns tuple
+
+        Args:
+            k: String
+            v: Float
+
+        Returns: Tuple
+    """
+    return (k, v * v)
---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+""" Make_multiplier function """
+
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+
+
+
+    def multi_func(n: float) -> float:
+
+        return (n * multiplier)
+    
+    return multi_func
