bc970e05f6d8b5f35c78b48ccffc9746b2274098
kevxcyj <kevcyjunior@gmail.com>
2025-05-10 21:38:03 -0500

    Added number 4/5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+""" Sum_list function """
+
+from typing import List
+
+def sum_list(imput_list: List[float]) -> float:
+    """ Adds list of arguments 
+    
+    Args:
+        input_list: list of floats
+
+    Returns:
+        Sum of all floats in input list
+    """
+
+    return sum(imput_list)
