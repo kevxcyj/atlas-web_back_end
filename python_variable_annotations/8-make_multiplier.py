15aab37bcbf5461c42c5a0855d3ce6328e0bd9ad
kevxcyj <kevcyjunior@gmail.com>
2025-05-10 23:20:45 -0500

    pycode

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -2,13 +2,12 @@
 
 """ Make_multiplier function """
 
+
 from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
 
     def multi_func(n: float) -> float:
-
         return (n * multiplier)
- 
     return multi_func
