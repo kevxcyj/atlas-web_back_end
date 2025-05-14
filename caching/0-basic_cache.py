962b2fe48c04d5ff5d46b5a96d7a6b459ace9f3e
kevxcyj <kevcyjunior@gmail.com>
2025-05-14 14:50:08 -0500

    Added more documentation

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -10,11 +10,13 @@ class BasicCache(BaseCaching):
     """ Inherits from BC and is a Caching system """
 
     def put(self, key, item):
+        """ Put the item in the cache """
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
+        """ Grab the item from the cache """
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data.get(key)
