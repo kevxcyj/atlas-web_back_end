8374d7056b6d99c1b37d0e76dd29abcf7bbf54f1
kevxcyj <kevcyjunior@gmail.com>
2025-05-21 12:32:20 -0500

    Added docu

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,6 +5,17 @@
 import re
 
 
-def filter_datum(fields: list[str], redaction: str, message: str, separator: str) -> str
+def filter_datum(fields: list[str], redaction: str, message: str, separator: str) -> str:
+    """ Returns log message obfuscated
+    
+    Args:
+        fields: List of fields
+        redaction: Obfuscating fields
+        message: Log line
+        separator: Separating all fields
+
+    Returns:
+        Log message with obfuscated string
+    """
     pattern = '|'.join(f'{field}{separator}.*?' for field in fields)
     return re.sub(pattern, lambda m: f'{m.group(0).split(separator)[0]}{separator}{redaction}', message)
