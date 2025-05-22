a8e10d9ce9e53cb8a59e1cb7c13f2747d27e68d4
kevxcyj <kevcyjunior@gmail.com>
2025-05-22 10:48:12 -0500

    Added missing quotation

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -15,13 +15,13 @@ def filter_datum(
     
     Args:
         fields: List of fields
-        redaction: Obfuscating fields
-        message: Log line
-        separator: Separating all fields
+        redaction <class str>: Obfuscating fields
+        message <class str>: Log line
+        separator <class str>: Separating all fields
 
     Returns:
         Log message with obfuscated string
     """
     pattern = '|'.join(f'{field}{separator}.*?' for field in fields)
     result = re.sub(pattern, lambda m: f'{m.group(0).split(separator)[0]}{separator}{redaction}', message)
-    return f"{result}
+    return f"{result}"
