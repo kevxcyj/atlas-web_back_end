99d4935217c7e303d6bb92ce7021ecf9677c4f84
kevxcyj <kevcyjunior@gmail.com>
2025-05-27 12:55:29 -0500

    Removed

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -4,7 +4,7 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
+from flask import Flask, jsonify, abort
 from flask_cors import (CORS, cross_origin)
 import os
 
