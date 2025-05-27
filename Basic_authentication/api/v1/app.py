55befe535ae71728d0db450bce62835277567c46
kevxcyj <kevcyjunior@gmail.com>
2025-05-27 12:53:43 -0500

    Removed tuple

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -15,7 +15,7 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 @app.errorhandler(404)
-def not_found(error) -> str | tuple:
+def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
