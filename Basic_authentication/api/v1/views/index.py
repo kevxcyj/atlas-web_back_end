b52ab7b23088c712639655281949a3500efbd104
kevxcyj <kevcyjunior@gmail.com>
2025-05-27 12:52:42 -0500

    Changed index

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """ Module of Index views
 """
-from flask import jsonify, abort, Response
+from flask import jsonify, abort
 from api.v1.views import app_views
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status() -> str | Response:
+def status() -> str:
     """ GET /api/v1/status
     Return:
       - the status of the API
