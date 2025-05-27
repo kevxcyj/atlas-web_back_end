24252211609ece33c017aad4fea7413e37deeba9
kevxcyj <kevcyjunior@gmail.com>
2025-05-27 13:42:59 -0500

    Fixeddocum

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -13,16 +13,16 @@ class Auth:
     Manage API authentication
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Flask request object
+        """ Checks if authentication is required
         """
         return False
 
     def authorization_header(self, request: Request = None) -> str:
-        """ Flask request objevt
+        """ Gets the authentication from the header
         """
         return None
 
     def current_user(self, request: Request = None) -> User:
-        """ Flask request object
+        """ Gets the current authenticated user
         """
         return None
