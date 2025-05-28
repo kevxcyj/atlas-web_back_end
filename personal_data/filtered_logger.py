8fa0dcb9f41d97a756a7c63819e8dc489341fd72
kevxcyj <kevcyjunior@gmail.com>
2025-05-28 11:46:50 -0500

    added docu

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -43,10 +43,12 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """ Init function """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """ Format function """
         message = super().format(record)
         return filter_datum(self.fields, self.REDACTION,
                             message, self.SEPARATOR)
