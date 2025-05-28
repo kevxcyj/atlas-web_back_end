c3e5ea32bf224f4eab4c358140d3639607ebb6be
kevxcyj <kevcyjunior@gmail.com>
2025-05-28 14:04:43 -0500

    Added is_valid

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -4,6 +4,11 @@
 import bcrypt
 
 def hash_password(password: str) -> bytes:
+    """ hash_password function """
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ is_valid function """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
