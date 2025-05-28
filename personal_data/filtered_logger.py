3947ddbc463ec2f09d771d37c95b7832c1e9aa68
kevxcyj <kevcyjunior@gmail.com>
2025-05-28 11:40:45 -0500

    updated fiklter

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 
 import re
 from typing import List
+import logging
 
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
@@ -26,3 +27,19 @@ def filter_datum(fields: List[str], redaction: str,
         repl: str = f"{field}={redaction}"
         message = re.sub(pattern, repl, message)
     return message
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, message, self.SEPARATOR)
