d4cff8b817dd13d51e67743c11c8d1ec54954d24
kevxcyj <kevcyjunior@gmail.com>
2025-05-28 14:05:25 -0500

    Added pycode

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -3,11 +3,13 @@
 
 import bcrypt
 
+
 def hash_password(password: str) -> bytes:
     """ hash_password function """
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """ is_valid function """
