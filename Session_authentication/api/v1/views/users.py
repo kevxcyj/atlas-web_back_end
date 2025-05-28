407805a568185525b8560174151751764f2aa64a
kevxcyj <kevcyjunior@gmail.com>
2025-05-27 23:27:17 -0500

    updated user

--- Session_authentication/api/v1/views/users.py
+++ Session_authentication/api/v1/views/users.py
@@ -25,6 +25,11 @@ def view_one_user(user_id: str = None) -> str:
       - User object JSON represented
       - 404 if the User ID doesn't exist
     """
+    if user_id == 'me':
+        if request.current_user is None:
+            abort(404)
+        return jsonify(request.current_user.to_json())
+    
     if user_id is None:
         abort(404)
     user = User.get(user_id)
@@ -120,3 +125,4 @@ def update_user(user_id: str = None) -> str:
         user.last_name = rj.get('last_name')
     user.save()
     return jsonify(user.to_json()), 200
+
