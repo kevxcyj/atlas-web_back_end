5e260ac6e5360343c2b3828ac0030b35eb176343
kevxcyj <kevcyjunior@gmail.com>
2025-05-28 10:51:13 -0500

    Changed code

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -1,35 +1,36 @@
 #!/usr/bin/env python3
 """ Session auth module """
 
-from flask import Blueprint, request, jsonify
+from typing import Optional
+from flask.typing import ResponseReturnValue
+from api.v1.views import app_views
+from flask import Response, abort, jsonify, request
 from models.user import User
 
-bp = Blueprint('session_auth', __name__)
 
-@bp.route('/auth_session/login', methods=['POST'])
-def login():
-    """ Def login function """
-    email = request.form.get('email')
-    password = request.form.get('password')
+@app_views.route('/auth_session/login', methods=['POST'])
+def user_login() -> ResponseReturnValue:
+    """ Login function
+    """
+    email: Optional[str] = request.form.get("email")
+    password: Optional[str] = request.form.get("password")
 
     if not email:
         return jsonify({"error": "email missing"}), 400
-    
     if not password:
         return jsonify({"error": "password missing"}), 400
-    
-    user = User.search(email=email)
-    
-    if not user:
+
+    users: list[User] = User.search({"email": email})
+    if not users:
         return jsonify({"error": "no user found for this email"}), 404
-    
-    if not user.is_valid_password(password):
-        return jsonify({"error": "wrong password"}), 401
-    
-    from api.v1.app import auth
-    session_id = auth.create_session(user.id)
-    
-    response = jsonify(user.to_json())
-    response.set_cookie('session', session_id, httponly=True)
-    
-    return response
+
+    for user in users:
+        if user.is_valid_password(password):
+            from api.v1.app import auth
+            session_id: str = auth.create_session(user.id)  # type: ignore
+            response: Response = jsonify(user.to_json())
+            response.set_cookie(str(os.getenv("SESSION_NAME")), session_id)
+            return response
+
+    # no user in users matched password
+    return jsonify({"error": "wrong password"}), 401
