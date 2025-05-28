bf99be9950ee748cce8d974049f25383c3e32c0f
kevxcyj <kevcyjunior@gmail.com>
2025-05-28 11:43:44 -0500

    pycode

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -43,5 +43,5 @@ class RedactingFormatter(logging.Formatter):
 
     def format(self, record: logging.LogRecord) -> str:
         message = super().format(record)
-        return filter_datum(self.fields, self.REDACTION, 
+        return filter_datum(self.fields, self.REDACTION,
                             message, self.SEPARATOR)
