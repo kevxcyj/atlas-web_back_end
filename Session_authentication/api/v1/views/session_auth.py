1bb663c4ba15258c6b23226e27d5959af3f01d00
kevxcyj <kevcyjunior@gmail.com>
2025-05-28 11:23:30 -0500

    added init

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -5,6 +5,7 @@ from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models.user import User
 from os import getenv
+import json
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
@@ -35,3 +36,25 @@ def login() -> str:
             response.set_cookie(getenv('SESSION_NAME'), sessionID)
             return response
     return jsonify({"error": "wrong password"}), 401
+
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """ Logout function """
+    request = request
+    if request is None:
+        return False
+    
+    session_id = request.cookies.get(getenv('SESSION_NAME'))
+    if not session_id:
+        return False
+    
+    from api.v1.app import auth
+    user_id = auth.user_id_for_session_id(session_id)
+    if not user_id:
+        return False
+    
+    auth.destroy_session(request)
+    
+    return jsonify({}), 200
