3f5da299f7eb40107c80c2e71a38fce7d53c679b
kevxcyj <kevcyjunior@gmail.com>
2025-05-27 20:21:46 -0500

    Added more docu

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -7,12 +7,6 @@ Auth module
 from typing import List, TypeVar
 from flask import Request
 
-"""
-Flask Request class.
-
-This class represents an HTTP request. It contains attributes and methods
-to interact with the incoming request data.
-"""
 
 class Auth:
     """
@@ -20,15 +14,21 @@ class Auth:
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Checks if authentication is required
+        Return
+        - False
         """
         return False
 
     def authorization_header(self, request: Request = None) -> str:
         """ Gets the authentication from the header
+        Returns:
+        - None
         """
         return None
 
     def current_user(self, request = None) -> TypeVar("User"):
         """ Gets the current authenticated user
+        Returns:
+        - None
         """
         return None
