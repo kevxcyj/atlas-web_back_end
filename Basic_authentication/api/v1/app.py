98b49f5df1a9e896dacbfa1e0fef35d037413767
kevxcyj <kevcyjunior@gmail.com>
2025-05-27 21:50:08 -0500

    Updated app.py

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -4,7 +4,7 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 
@@ -13,6 +13,27 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+auth = None
+
+if os.environ.get('AUTH_TYPE') == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+@app.before_request
+def before_request():
+    global auth
+    if auth is None:
+        return
+    
+    if request.path in ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']:
+        return
+    
+    if auth.authorization_header(request) is None:
+        abort(401)
+    
+    if auth.current_user(request) is None:
+        abort(403)
 
 @app.errorhandler(404)
 def not_found(error) -> str:
--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -44,7 +44,9 @@ class Auth:
         Returns:
         - None
         """
-        return None
+        if request is None:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar("User"): # pyright: ignore
         """ Gets the current authenticated user
