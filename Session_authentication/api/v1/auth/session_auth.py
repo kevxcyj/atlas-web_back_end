cb2f597fc551aa94bcb2e694d12067722116aa94
kevxcyj <kevcyjunior@gmail.com>
2025-05-28 00:09:18 -0500

    added userid

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -19,6 +19,7 @@ class SessionAuth(Auth):
         return session_id
     
     def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ User id function """
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
