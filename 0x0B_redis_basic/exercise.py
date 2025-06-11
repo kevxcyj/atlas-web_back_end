0fa0e507457d687b2e4314346fc15e16a57dc850
kevxcyj <kevcyjunior@gmail.com>
2025-06-10 21:02:33 -0500

    Added get func and str int

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -15,3 +15,20 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn=None):
+        """ Retrieve value from Redis """
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn is not None:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """Returns value as string """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """Returns value as integer """
+        return self.get(key, fn=int)
