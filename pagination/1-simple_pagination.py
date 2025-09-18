bebfd09d516677b261df9fafd64f5664ae0add6b
kevxcyj <kevcyjunior@gmail.com>
2025-09-18 10:54:50 -0500

    pycode

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -26,17 +26,17 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        
+
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
         total_items = len(self.dataset())
-        
+
         start_index, end_index = index_range(page, page_size)
-        
+
         if start_index >= total_items:
             return []
-        
+
         return self.dataset()[start_index:end_index]
 
 
